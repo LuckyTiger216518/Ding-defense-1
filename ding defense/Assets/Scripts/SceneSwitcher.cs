efea871b3b0d0b8e8b8739d7f7882d1a7e0d66f3
LuckyTiger216518 <Sebastiandahl256@gmail.com>
2023-05-15 19:48:52 +0200

    added simpel path og added noget for alexander siden han ikke kan
    :+1:

--- ding defense/Assets/Scripts/SceneSwitcher.cs
+++ ding defense/Assets/Scripts/SceneSwitcher.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string targetscene = "ding-wip1";
+    public void SwitchScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(targetscene);
     }
 }
