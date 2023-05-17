79bb077c7f6218702bdaf50176f9a7b6117f24ff
KageMunken <tobiasdino.2@gmail.com>
2023-05-17 23:10:29 +0200

    Final
    Slettede alle test scripts, så den er klar til at aflevere

--- ding defense/Assets/Scripts/Enemies/FlyingEnemey.cs
+++ ding defense/Assets/Scripts/Enemies/FlyingEnemey.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class FlyingEnemy : MonoBehaviour
 {
+    //laver 2 intergers som skal bruges til at bestemme fjenders liv.
     public int maxHealth = 5;
     public int currentHealth;
 
     void Start()
     {
+        // sætter startværdien for fjendernes hp til maxHealth
         currentHealth = maxHealth;
     }
 
+    //TakeDamage får fjenderne til at tage skade og hvis deres liv bliver 0 eller mindre vil de dø (gameobjectet bliver slettet)
     public void TakeDamage(int amount)
     {
         currentHealth -= amount;
